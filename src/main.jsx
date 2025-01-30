eb69928bafdfc353b90fe5ab52a54d138f82badb
Tolga Buhur <tolbuhur@gmail.com>
2025-01-30 17:04:15 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './components/Greetings.jsx'
+import ItemList from './components/ItemList.jsx'
+import Timer from './components/Timer.jsx'
+import ToggleMessage from './components/ToggleMessage.jsx'
+import UserProfile from './components/UserProfile.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
